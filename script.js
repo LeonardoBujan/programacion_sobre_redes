2d4d3ea288953313aa5460b680b9e9986d57be9e
agroquinvet <maryangelin.quintero@gmail.com>
2024-09-18 19:57:31 -0300

    insertando contenido a la pagina protocolo

--- script.js
+++ script.js
@@ -1,12 +1,48 @@
 
     // Cuando el modal se cierra, detiene el video
-    var modalVLAN = document.getElementById('modaDispositivos');
-    modalVLAN.addEventListener('hidden.bs.modal', function () {
+    var modalDispositivos = document.getElementById('modaDispositivos');
+    Dispositivos.addEventListener('hidden.bs.modal', function () {
         var iframe = document.getElementById('videoDispositivos');
         iframe.src = iframe.src;  // Reinicia el iframe al cerrar el modal, deteniendo el video
     });
-    var modalVLAN = document.getElementById('modalConexioness');
-    modalVLAN.addEventListener('hidden.bs.modal', function () {
+    var modalConexiones = document.getElementById('modalConexioness');
+    modalConexiones.addEventListener('hidden.bs.modal', function () {
         var iframe = document.getElementById('videoconexiones');
+        iframe.src = iframe.src;  
+    });
+
+    var modalSpannig = document.getElementById('modalSpannig');
+    modalSpannig.addEventListener('hidden.bs.modal', function () {
+        var iframe = document.getElementById('videoSpanningtree');
+        iframe.src = iframe.src;  
+    });
+
+    var modalOspf = document.getElementById('modalOspf');
+    modalOspf.addEventListener('hidden.bs.modal', function () {
+        var iframe = document.getElementById('videoOspf');
+        iframe.src = iframe.src;  
+    });
+
+    var modalArp = document.getElementById('modalArp');
+    modalArp.addEventListener('hidden.bs.modal', function () {
+        var iframe = document.getElementById('videoArp');
+        iframe.src = iframe.src;  
+    });
+
+    var modalCorreo = document.getElementById('modalCorreo');
+    modalArp.addEventListener('hidden.bs.modal', function () {
+        var iframe = document.getElementById('videoCorreo');
+        iframe.src = iframe.src;  
+    });
+
+    var modal8023 = document.getElementById('modal8023');
+    modal8023.addEventListener('hidden.bs.modal', function () {
+        var iframe = document.getElementById('video8023');
+        iframe.src = iframe.src;  
+    });
+
+    var modalOsi = document.getElementById('modalOsi');
+    modalOsi.addEventListener('hidden.bs.modal', function () {
+        var iframe = document.getElementById('videoOsi');
         iframe.src = iframe.src;  
     });
