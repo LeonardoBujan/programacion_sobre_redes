7e412def74451cb8d1cba381208644748cf2a518
agroquinvet <maryangelin.quintero@gmail.com>
2024-09-18 21:28:59 -0300

    integrando la pagina de certificaciones y osluciones

--- script.js
+++ script.js
@@ -1,48 +1,81 @@
-
-    // Cuando el modal se cierra, detiene el video
+document.addEventListener('DOMContentLoaded', function () {
     var modalDispositivos = document.getElementById('modaDispositivos');
-    Dispositivos.addEventListener('hidden.bs.modal', function () {
-        var iframe = document.getElementById('videoDispositivos');
-        iframe.src = iframe.src;  // Reinicia el iframe al cerrar el modal, deteniendo el video
-    });
+    if (modalDispositivos) {
+        modalDispositivos.addEventListener('hidden.bs.modal', function () {
+            var iframe = document.getElementById('videoDispositivos');
+            if (iframe) {
+                iframe.src = iframe.src;  // Reinicia el iframe al cerrar el modal, deteniendo el video
+            }
+        });
+    }
+
     var modalConexiones = document.getElementById('modalConexioness');
-    modalConexiones.addEventListener('hidden.bs.modal', function () {
-        var iframe = document.getElementById('videoconexiones');
-        iframe.src = iframe.src;  
-    });
+    if (modalConexiones) {
+        modalConexiones.addEventListener('hidden.bs.modal', function () {
+            var iframe = document.getElementById('videoconexiones');
+            if (iframe) {
+                iframe.src = iframe.src;  
+            }
+        });
+    }
 
     var modalSpannig = document.getElementById('modalSpannig');
-    modalSpannig.addEventListener('hidden.bs.modal', function () {
-        var iframe = document.getElementById('videoSpanningtree');
-        iframe.src = iframe.src;  
-    });
+    if (modalSpannig) {
+        modalSpannig.addEventListener('hidden.bs.modal', function () {
+            var iframe = document.getElementById('videoSpanningtree');
+            if (iframe) {
+                iframe.src = iframe.src;  
+            }
+        });
+    }
 
     var modalOspf = document.getElementById('modalOspf');
-    modalOspf.addEventListener('hidden.bs.modal', function () {
-        var iframe = document.getElementById('videoOspf');
-        iframe.src = iframe.src;  
-    });
+    if (modalOspf) {
+        modalOspf.addEventListener('hidden.bs.modal', function () {
+            var iframe = document.getElementById('videoOspf');
+            if (iframe) {
+                iframe.src = iframe.src;  
+            }
+        });
+    }
 
     var modalArp = document.getElementById('modalArp');
-    modalArp.addEventListener('hidden.bs.modal', function () {
-        var iframe = document.getElementById('videoArp');
-        iframe.src = iframe.src;  
-    });
+    if (modalArp) {
+        modalArp.addEventListener('hidden.bs.modal', function () {
+            var iframe = document.getElementById('videoArp');
+            if (iframe) {
+                iframe.src = iframe.src;  
+            }
+        });
+    }
 
     var modalCorreo = document.getElementById('modalCorreo');
-    modalArp.addEventListener('hidden.bs.modal', function () {
-        var iframe = document.getElementById('videoCorreo');
-        iframe.src = iframe.src;  
-    });
+    if (modalCorreo) {
+        modalCorreo.addEventListener('hidden.bs.modal', function () {
+            var iframe = document.getElementById('videoCorreo');
+            if (iframe) {
+                iframe.src = iframe.src;  
+            }
+        });
+    }
 
     var modal8023 = document.getElementById('modal8023');
-    modal8023.addEventListener('hidden.bs.modal', function () {
-        var iframe = document.getElementById('video8023');
-        iframe.src = iframe.src;  
-    });
+    if (modal8023) {
+        modal8023.addEventListener('hidden.bs.modal', function () {
+            var iframe = document.getElementById('video8023');
+            if (iframe) {
+                iframe.src = iframe.src;  
+            }
+        });
+    }
 
     var modalOsi = document.getElementById('modalOsi');
-    modalOsi.addEventListener('hidden.bs.modal', function () {
-        var iframe = document.getElementById('videoOsi');
-        iframe.src = iframe.src;  
-    });
+    if (modalOsi) {
+        modalOsi.addEventListener('hidden.bs.modal', function () {
+            var iframe = document.getElementById('videoOsi');
+            if (iframe) {
+                iframe.src = iframe.src;  
+            }
+        });
+    }
+});
